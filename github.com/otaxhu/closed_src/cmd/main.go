1e52ce29f43520be4542a77cbf6eb9b68a18e8ec
otaxhu <oscarperniamoreno@gmail.com>
2024-07-19 21:28:27 -0400

    go source files created

---
+++ github.com/otaxhu/closed_src/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/otaxhu/closed_src/interfaces"
+)
+
+func main() {
+	println(interfaces.Add(1, 2))
+}
---
+++ github.com/otaxhu/closed_src/interfaces/lib.go
@@ -0,0 +1,5 @@
+package interfaces
+
+import _ "github.com/otaxhu/closed_src/closed_src" // provides Add
+
+func Add(a, b int) int
